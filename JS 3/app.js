b9623d7557c523a85fab22b65fb45ca5080799a3
Saeed Ahmed <saeedid968@gmail.com>
2025-02-21 04:28:22 +0500

    JS assignment 3

---
+++ JS 3/app.js
@@ -0,0 +1,25 @@
+////// CHAPTER 9-11 USER INPUT & CONDITIONAL STATEMENT //////
+
+
+// Question No 1 
+
+var city = prompt("Enter city name..");
+if (city === "Karachi") {
+    alert("Welcone to the city of Lights");
+}
+
+
+// Question No 2
+
+var gender = prompt("Enter your gender please!")
+if ( gender === "Male" ) {
+    alert("WELCOME SIR!")
+}
+if ( gender === "Female" ) {
+    alert("WELCOME MA'AM!")
+}
+
+
+// Question No 3
+
+
